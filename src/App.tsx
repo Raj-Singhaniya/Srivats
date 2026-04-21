0d93d6211ead1fd9699e106ec12825800a31f724
Raj-Singhaniya <rajsinghaniya290804@gmail.com>
2026-04-22 03:34:51 +0530

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      
+      {/* 👇 ADD basename HERE */}
+      <BrowserRouter basename="/Srivats">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
